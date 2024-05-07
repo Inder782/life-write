c943652ea42c9d53dab68a2077017d1e71166722
Inder Singh chandel <indersinghchandel@Inders-MacBook-Air.local>
2024-05-07 23:55:57 +0530

    feat(navbar):completed navbar and theme

--- components/ThemeSelect/Theme.tsx
+++ components/ThemeSelect/Theme.tsx
@@ -3,19 +3,30 @@ import React from "react";
 import { Switch } from "../ui/switch";
 import { Label } from "../ui/label";
 import { useTheme } from "@/context/ThemeContext";
+import Image from "next/image";
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
-  const handlechange = () => {
-    setMode("light");
-  };
+
+  const handleTheme=()=>{
+    if (localStorage.theme=="dark"){
+      setMode("light")
+      localStorage.theme="light"
+    }    
+    else{
+      setMode("dark")
+      localStorage.theme="dark"
+      
+    }
+  }
+
   return (
     <div className="bg-black flex items-center justify-between p-2">
       <div className="flex items-center space-x-2">
-        <Switch id="dark_mode" onClick={handlechange} />
-        <Label htmlFor="dark_mode" className="text-white ">
-          Dark Mode
-        </Label>
+        <Switch id="dark_mode" onClick={handleTheme}/>
+       {mode ==="light" ?(<Image src="/sun.svg" width={50} height={255} alt="sun" className="invert"/>)
+       :
+       ((<Image src="/moon.svg" width={50} height={255} alt="sun" className="invert"/>))}
       </div>
     </div>
   );
