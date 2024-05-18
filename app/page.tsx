c2e4cc05b5a99a0da2e1f7a9ac2de46d15ccb552
Inder Singh chandel <indersinghchandel@Inders-MacBook-Air.local>
2024-05-18 15:10:46 +0530

    dynamic route for blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import { Card } from "@/components/Card";
 import { Blogs } from "@/lib/Blogs";
+import Link from "next/link";
 import React from "react";
 
 // for now static will make dynamic once backend work is done
 const page = async () => {
   const blogs = await Blogs();
 
-  console.log(blogs);
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
       {blogs?.map((item) => (
+        <div>
+        <Link href={`post/${item.id}`}>
         <Card key={item.id} title={item.title} />
+        </Link>
+        </div>
       ))}
     </div>
   );
