93448ac8439051a399ddb497281feccbd1d5c5f5
inder782 <inderchandel782@gmail.com>
2024-05-18 13:37:13 +0530

    feat(all_blogs):prisma and servre action for all blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { Card } from "@/components/Card";
+import { Blogs } from "@/lib/Blogs";
 import React from "react";
 
 // for now static will make dynamic once backend work is done
-const page = () => {
+const page = async () => {
+  const blogs = await Blogs();
+
+  console.log(blogs);
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {blogs?.map((item) => (
+        <Card key={item.id} title={item.title} />
+      ))}
     </div>
   );
 };
