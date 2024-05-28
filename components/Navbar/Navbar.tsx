b66dc8b528f3652a63fef4e7fa7861cd0f65b04b
inder782 <inderchandel782@gmail.com>
2024-05-29 05:21:14 +0530

    feat(nav):sheet open links

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -17,8 +17,10 @@ const Navbar = () => {
     <div className="bg-black flex items-center justify-between p-2">
       <Theme />
       <div>
-        <Link href="/">
-          <h1 className="text-white font-bold text-3xl ml-32">Inder Chandel</h1>
+        <Link href="/" className="max-sm:text-1xl">
+          <h1 className="text-white font-bold text-3xl ml-32 ">
+            Inder Chandel
+          </h1>
         </Link>
       </div>
       <div className="text-white flex max-[1300px]:hidden">
@@ -33,14 +35,12 @@ const Navbar = () => {
           <SheetTrigger>
             <FcLike />
           </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
-              <SheetDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </SheetDescription>
-            </SheetHeader>
+          <SheetContent className="flex flex-col items-center ">
+            {Links_Nav.map((item) => (
+              <Link href={item.to} className="p-6 mt-20" key={item.id}>
+                {item.Link}
+              </Link>
+            ))}
           </SheetContent>
         </Sheet>
       </div>
