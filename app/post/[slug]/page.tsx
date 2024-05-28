b02222a85636b95a28fc3462b2383e6bce2b1eea
inder782 <inderchandel782@gmail.com>
2024-05-29 04:59:33 +0530

    feat(blogs):read blog page and tiny improvements

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -4,14 +4,22 @@ import Image from "next/image";
 export default async function Page({ params }: { params: { slug: string } }) {
   const id = parseInt(params.slug);
   const data = await Blog_Content(id);
-
+  if (!data) {
+    return <div>Blog not found</div>;
+  }
   return (
-    <div className="flex flex-col items-center justify-center mt-10">
-      <Image src={data?.image} width={500} height={500} alt="thumbnail" />
-      <div className="text-4xl font-bold ">
-        <h1>{data?.title}</h1>
+    <div className="flex flex-col items-center justify-center  w-1/2 mx-auto lg:w-82 max-md:w-96 shadow-md">
+      <div className="p-8 w-full">
+        <h1 className="text-4xl font-bold text-left ">{data?.title}</h1>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: data!.body }} className="p-16" />
+      <Image
+        src={data?.image!}
+        width={850}
+        height={200}
+        alt="thumbnail"
+        className="mt-2"
+      />
+      <div dangerouslySetInnerHTML={{ __html: data!.body }} className="p-10" />
     </div>
   );
 }
